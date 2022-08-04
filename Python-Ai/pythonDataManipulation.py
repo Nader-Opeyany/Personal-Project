fac71d0ba541b62088ba4be247af70aa1c4cf712
naderopeyany <naderopeyany@gmail.com>
2022-08-04 12:14:03 -0500

    com

--- Python-Ai/pythonDataManipulation.py
+++ Python-Ai/pythonDataManipulation.py
@@ -1,3 +1,8 @@
 import numpy as np
-listTest = [1,2,3,4,5,6]
-print(listTest[1:6:2])
+
+my_dict={0: 'XYZ', 1: 'XYZ', 2: 'XYZ', 3: 'XYZ', 4: 'XYZ', 5: 'XYZ', 6: 'XYZ', 7: 'XYZ', 8: 'XYZ', 9: 'XYZ', 10:'XYZ',11:'XYZ',12:'XYZ',13:'XYZ',14:'XYZ'}
+
+
+listTest = list(my_dict.items())
+
+print(listTest[0:9:3])
