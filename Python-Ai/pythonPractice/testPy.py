550b4728c4ff3a71d36f39689ac2f141b3603340
naderopeyany <naderopeyany@gmail.com>
2022-08-21 18:27:06 -0500

    test finsihed

--- Python-Ai/pythonPractice/testPy.py
+++ Python-Ai/pythonPractice/testPy.py
@@ -5,7 +5,9 @@ from sqlalchemy import true
 
 class TestClass:
     def __init__(self,test, strTest,Bigboolean) :
+        #This is a list containing 
         self.test = test
+        self.hexCheck = self.test[0]
         self.strTest = strTest
         self.Bigboolean = Bigboolean
     def getTestName(self):
@@ -14,9 +16,11 @@ class TestClass:
         return self.strTest
     def getTestResult(self):
         return self.Bigboolean
+
 class CheckBitValue(TestClass):
     def executeTest(self):
-        print(self.strTest)
+        if(self.hexCheck[self.test[2]] == str(self.test[1])):
+            print(self.hexCheck)
         
-checkthis = CheckBitValue(["1",1,1],"1",true)
+checkthis = CheckBitValue(["11110000",1,3],"1",true)
 checkthis.executeTest()
