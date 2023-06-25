212d96ae63c7708bce1998e6dfb5123f62dc79bb
naderopeyany <naderopeyany@gmail.com>
2023-06-25 12:34:06 -0700

    commit

--- Software_Fuzz_Testing/add.js
+++ Software_Fuzz_Testing/add.js
@@ -1,7 +1,29 @@
-import { createVendiaClient } from '@vendia/client'
+import { createVendiaClient } from '@vendia/client';
 
 const client = createVendiaClient({
   apiUrl: `https://r4a3jwtij7.execute-api.us-west-1.amazonaws.com/graphql/`,
   websocketUrl: `wss://sfsug601zd.execute-api.us-west-1.amazonaws.com/graphql`,
-  apiKey: process.env.VENDIA_API_KEY, ESWp6bZVHE88YRqXwzFpbbTpmDtEQYe8t9fv15AjPYk8// <---- API key
+  apiKey:  'ESWp6bZVHE88YRqXwzFpbbTpmDtEQYe8t9fv15AjPYk8',// <---- API key
+
+  
+});
+const { entities } = client
+
+async function add(){
+  // Add a new "product"
+const before = Date.now();
+const addResponse = await entities.shoppingList.add({
+  item: 'for demo',
+  recommendedLocation:'for demoTwo'
+},
+{
+  synchMode: 'ASYNC'
 })
+const after = Date.now()
+console.log(`This is elapsed Time: ${(after-before)/1000} seconds`)
+
+};
+
+
+//Calling functions to run here
+add();
