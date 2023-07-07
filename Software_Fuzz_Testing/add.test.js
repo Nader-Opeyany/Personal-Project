06b916346e054b9091221f90b2e9041e68f92483
naderopeyany <naderopeyany@gmail.com>
2023-07-07 11:00:39 -0700

    final

--- Software_Fuzz_Testing/add.test.js
+++ Software_Fuzz_Testing/add.test.js
@@ -1,14 +1,15 @@
 const add = require('./add')
 const  superRandom  = require('./generateMalformedInput');
 
-//variable declerations
-var itemObj =  superRandom(10);
-var recommendedLocationObj = superRandom(10);
+for( i = 0;i<10<i++;){
+  //variable declerations
+  var itemObj =  superRandom(10);
+  var recommendedLocationObj = superRandom(10);
 
-//jest functionality
-test('Working code UwU', () => {
-  return add(itemObj,recommendedLocationObj).then(data => {
-    expect(data.result.item).toBe(itemObj);
+  //jest functionality
+  test('Working code UwU', () => {
+    return add(itemObj,recommendedLocationObj).then(data => {
+      expect(data.result.item).toBe(itemObj);
+    });
   });
-});
-  
+};
